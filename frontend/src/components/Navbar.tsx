898c06d3d6eb2188a018866cb0b3fa5e14e7b28d
Byznes <dominik.sukar@gmaik.com>
2024-03-21 23:43:47 +0100

    Added icons to Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,16 +3,18 @@ import { Link, useLocation  } from 'react-router-dom'
 
 import Logo from './Logo'
 
-const Nav = ({name, path, currentPath, changeCurrentPath}:{name: string, path: string, currentPath: string, changeCurrentPath: Dispatch<React.SetStateAction<string>>}) => {
-  let active: boolean = false;
-  if (currentPath === path) {
-    active = true;
-  }
-  const router = useLocation();
+import HomeIcon from '../assets/home.svg'
+import SettingsIcon from '../assets/settings.svg'
+import DataIcon from '../assets/database.svg'
+import LogoutIcon from '../assets/logout.svg'
+
+const Nav = ({name, path, currentPath, changeCurrentPath, icon}:{name: string, path: string, currentPath: string, changeCurrentPath: Dispatch<React.SetStateAction<string>>, icon:string}) => {
     return (
             <Link to={path}>
-                <button type="button" className="w-full py-2 bg-1 text-white hover:bg-2 transition-colors"
-                onClick={()=>changeCurrentPath(name)}>{ name }
+                <button type="button" className="flex w-full py-2 bg-1 text-white hover:bg-2 transition-colors"
+                onClick={()=>changeCurrentPath(name)}>
+                  <img src={icon} alt="Home logo" className='mr-2 ml-20'></img>
+                  <div>{ name }</div>
                 </button>
             </Link>
         )
@@ -26,9 +28,10 @@ const Navbar = () => {
     <div className="fixed left-0 top-0 h-full w-64 bg-1">
         <div className='flex flex-col'>
             <Logo></Logo>
-            <Nav name={"Home"} currentPath={currentPath} changeCurrentPath={changeCurrentPath} path="/"></Nav>
-            <Nav name={"Data"} currentPath={currentPath} changeCurrentPath={changeCurrentPath} path="/measurements"></Nav>
-            <Nav name={"Settings"} currentPath={currentPath} changeCurrentPath={changeCurrentPath} path="/settings"></Nav>
+            <Nav name={"Home"} currentPath={currentPath} changeCurrentPath={changeCurrentPath} icon={HomeIcon} path="/"></Nav>
+            <Nav name={"Data"} currentPath={currentPath} changeCurrentPath={changeCurrentPath} icon={SettingsIcon} path="/measurements"></Nav>
+            <Nav name={"Settings"} currentPath={currentPath} changeCurrentPath={changeCurrentPath} icon={DataIcon} path="/settings"></Nav>
+            <Nav name={"Logout"} currentPath={currentPath} changeCurrentPath={changeCurrentPath} icon={LogoutIcon} path="/"></Nav>
         </div>
     </div>
   )
